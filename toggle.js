68023c79f77ba8c90bec3bf4fe9a66cbeafd36f4
qua3k <78624738+qua3k@users.noreply.github.com>
2021-07-08 17:24:08 +0000

    Automatically detect dark mode
    Uses prefers-color-scheme but allows the button to override it

--- toggle.js
+++ toggle.js
@@ -1,16 +1,21 @@
 function setDarkMode() {
-  if (localStorage.getItem('mode') === 'dark') {
-    localStorage.setItem('mode', 'white');
-    document.body.classList.remove('dark-mode');
-  } else {
-    localStorage.setItem('mode', 'dark');
-    document.body.classList.add('dark-mode');
-  }
+    if (localStorage.getItem("mode") === "dark") {
+        localStorage.setItem("mode", "white");
+        document.body.classList.remove("dark-mode");
+    } else {
+        localStorage.setItem("mode", "dark");
+        document.body.classList.add("dark-mode");
+    }
 }
 
-if (localStorage.getItem('mode') === 'dark') {
-  document.body.classList.add('dark-mode');
+function detectDarkMode() {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches && localStorage.getItem("mode") === null) {
+        localStorage.setItem("mode", "dark");
+    } if (localStorage.getItem("mode") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
 }
+detectDarkMode();
 
 const button = document.querySelector('button');
 
